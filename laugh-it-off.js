d96b455d39a998b89850f2a6bf457b8cfbea825c
David Blackwelder <dblackw2@gmail.com>
2018-07-22 10:56:06 -0400

    Adding Functions programming quizes

---
+++ laugh-it-off.js
@@ -0,0 +1,14 @@
+/*
+ * Programming Quiz: Laugh it Off 1 (5-1)
+ * 
+ * Directions:
+ * Declare a function called laugh() that returns "hahahahahahahahahaha!". Print the value returned from the laugh() function to the console.
+ */
+
+// your code goes here
+function laugh() {
+    let silly = "hahahahahahahahahaha";
+    return silly + "!";
+}
+
+console.log(laugh());
