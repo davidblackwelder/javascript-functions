822540b8f364ed83d04b94337a447066043a537e
David Blackwelder <dblackw2@gmail.com>
2018-07-22 23:19:10 -0400

    Updated Cry programming quiz

---
+++ cry.js
@@ -0,0 +1,13 @@
+/*
+ * Programming Quiz: Cry (5-5)
+ * 
+ * Directions:
+Write a named function expression that stores the function in a variable called cry and returns "boohoo!". Don't forget to call the function using the variable name, not the function name:
+ */
+
+// your code goes here
+let cry = function sad() {
+    return "boohoo!";
+};
+
+console.log(cry()); // prints "boohoo!"
